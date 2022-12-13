0f2faff5a9f195b6fd6d36c46b179e747d8631a6
Darwin <darwin78913@gmail.com>
2022-12-13 16:24:25 -0500

    add hex header

--- hex.h
+++ hex.h
@@ -1,6 +1,12 @@
-#ifndef __COMMON_H__
-#define __COMMON_H__
+#ifndef __HEX_GUARD
+#define __HEX_GUARD
 //header file content
 
+class Hex {
+    public:
+        int q, r, s;
+
+        bool operator==(const Hex& hex) const;
+};
 
 #endif
