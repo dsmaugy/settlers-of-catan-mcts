8e15bb13d81075cf17289b6100b1f19538387e9c
rafdkg <rdavila754@gmail.com>
2022-12-13 18:00:48 -0500

    building player

--- catan_game.h
+++ catan_game.h
@@ -3,7 +3,10 @@
 //header file content
 #include <unordered_set>
 #include <unordered_map>
+#include <vector>
+
 #include "catan_player.h"
+#include "hex.h"
 
 class GameState{
 
@@ -13,12 +16,12 @@ public:
     Player current_turn;
     Hex robber_position;
 
-    static unordered_set <Hex> tiles;
-    static unordered_map <Hex, int> tile_rewards;
+    static std::unordered_set <Hex> tiles;
+    static std::unordered_map <Hex, int> tile_rewards;
  
     bool is_game_over();
     Player game_winner();
-    Vector<GameState> get_all_moves();
+    std::vector<GameState> get_all_moves();
 };
 
 class Game {
--- catan_player.h
+++ catan_player.h
@@ -1,6 +1,20 @@
-#ifndef __COMMON_H__
-#define __COMMON_H__
+#ifndef __CATAN_PLAYER_H__
+#define __CATAN_PLAYER_H__
+
+#include <unordered_set>
+#include "hex.h"
+
 //header file content
 
+
+class Player {
+
+public:
+    PlayerPolicy player_policy;
+
+    static std::unordered_set<std::pair<(Hex, Hex)> roads;
+
+
+};
 
 #endif
