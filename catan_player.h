a5bbc8ad8af56186541d3a09b2b0fb9babbb055c
Darwin <darwin78913@gmail.com>
2022-12-13 16:02:36 -0500

    added some header files

---
+++ catan_player.h
@@ -0,0 +1,6 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+//header file content
+
+
+#endif
--- run_catan_agent.cpp
+++ run_catan_agent.cpp
@@ -4,6 +4,5 @@ using namespace std;
 
 int main(int argc, char** argv) {
     cout << "hello world" << "df\n";
-
     return 0;
 }
